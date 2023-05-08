daf20678e46f8fe8f46bf55fa792f572db5c23c7
JeongMin <skawjdals96@gmail.com>
2023-05-08 22:15:27 +0900

    first commit

---
+++ src/main/java/com/toyboard/jeongmin/JeongminApplication.java
@@ -0,0 +1,13 @@
+package com.toyboard.jeongmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeongminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeongminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyboard/jeongmin/JeongminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyboard.jeongmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
