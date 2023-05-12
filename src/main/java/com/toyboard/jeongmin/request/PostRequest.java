eda67b65a92754d77af2f6fb865b2049d5c64b64
JeongMin <skawjdals96@gmail.com>
2023-05-12 23:56:44 +0900

    Feat: 생성시간을 요청 Dto에 생성

--- src/main/java/com/toyboard/jeongmin/request/PostRequest.java
+++ src/main/java/com/toyboard/jeongmin/request/PostRequest.java
@@ -2,12 +2,17 @@ package com.toyboard.jeongmin.request;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostRequest {
 
     private String title;
 
     private String content;
+
+    private LocalDateTime regTime;
+
 
 
 }
