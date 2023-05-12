3b29927f0f286d09d5f1b9d6883c55007e6d42e3
JeongMin <skawjdals96@gmail.com>
2023-05-13 01:18:08 +0900

    Feat: 100개로 제한된 전체 게시글 리스트와 검색을 위한 NamedQuery 사용

--- src/main/java/com/toyboard/jeongmin/repository/PostRepository.java
+++ src/main/java/com/toyboard/jeongmin/repository/PostRepository.java
@@ -1,8 +1,16 @@
 package com.toyboard.jeongmin.repository;
 
 import com.toyboard.jeongmin.domain.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findByTitleContainingOrderByRegTimeDesc(String keyword, Pageable pageable);
+
+    @Query("select p from Post p")
+    List<Post> findAllPostsLimited100(Pageable pageable);
 }
