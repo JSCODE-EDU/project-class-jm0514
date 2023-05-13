da2e006daf038b50152315f1607df6b513e7fc95
JeongMin <skawjdals96@gmail.com>
2023-05-13 23:55:57 +0900

    Feat: Named Query에서 @Query 애노테이션을 사용하는 방식으로 변경

--- src/main/java/com/toyboard/jeongmin/repository/PostRepository.java
+++ src/main/java/com/toyboard/jeongmin/repository/PostRepository.java
@@ -4,12 +4,14 @@ import com.toyboard.jeongmin.domain.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findByTitleContainingOrderByRegTimeDesc(String keyword, Pageable pageable);
+    @Query("select p from Post p where p.title like %:keyword%")
+    List<Post> findByTitleKeyword(@Param("keyword")String keyword, Pageable pageable);
 
     @Query("select p from Post p")
     List<Post> findAllPostsLimited100(Pageable pageable);
