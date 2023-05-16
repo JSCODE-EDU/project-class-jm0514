6e07ba3395a656b710a1d832b89dde67c5f41c62
JeongMin <skawjdals96@gmail.com>
2023-05-16 20:27:21 +0900

    Refactor: API 스펙에 Entity가 노출되지 않게 수정

--- src/main/java/com/toyboard/jeongmin/controller/PostController.java
+++ src/main/java/com/toyboard/jeongmin/controller/PostController.java
@@ -5,7 +5,6 @@ import com.toyboard.jeongmin.request.PostRequest;
 import com.toyboard.jeongmin.response.PostResponse;
 import com.toyboard.jeongmin.service.PostService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,26 +12,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/boards")
 @RequiredArgsConstructor
-@Slf4j
 public class PostController {
 
     private final PostService postService;
 
     @GetMapping("/{postId}")
-    public Post getPost(@PathVariable Long postId) {
+    public PostResponse getPost(@PathVariable Long postId) {
         return postService.findPost(postId);
     }
 
     @PostMapping
     public PostResponse createPost(@RequestBody PostRequest postRequest){
-        Post post = postService.writePost(postRequest);
-        return new PostResponse(post);
+        return postService.writePost(postRequest);
     }
 
     @PatchMapping("/{postId}")
     public PostResponse modifyPost(@PathVariable Long postId, @RequestBody PostRequest postRequest) {
-        Post post = postService.modifyPost(postId, postRequest);
-        return new PostResponse(post);
+        return postService.modifyPost(postId, postRequest);
     }
 
     @DeleteMapping("/{postId}")
