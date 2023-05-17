7680f230ec7e408ea59a97aab05be24ab24e8804
JeongMin <skawjdals96@gmail.com>
2023-05-17 18:02:42 +0900

    Feat: 유효성 검사를 위한 Validation 추가

--- src/main/java/com/toyboard/jeongmin/request/PostRequest.java
+++ src/main/java/com/toyboard/jeongmin/request/PostRequest.java
@@ -1,18 +1,23 @@
 package com.toyboard.jeongmin.request;
 
+import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Getter
 public class PostRequest {
 
+    @NotBlank(message = "제목은 1자이상 15자 이하여야 합니다.")
     private String title;
 
+    @NotBlank(message = "내용은 1자이상 1000자 이하여야 합니다.")
     private String content;
 
-    private LocalDateTime regTime;
+    private String error_message;
 
+    private LocalDateTime regTime;
 
 
 }
