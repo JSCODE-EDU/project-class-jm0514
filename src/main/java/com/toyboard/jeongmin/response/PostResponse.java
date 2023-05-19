5fe37fb640e4be9eb981730a50e73059b4ea2048
JeongMin <skawjdals96@gmail.com>
2023-05-19 23:34:04 +0900

    Feat: 응답 형태, 각 파라미터 데이터 타입, 각 파라미터의 의미 표시

--- src/main/java/com/toyboard/jeongmin/response/PostResponse.java
+++ src/main/java/com/toyboard/jeongmin/response/PostResponse.java
@@ -1,6 +1,7 @@
 package com.toyboard.jeongmin.response;
 
 import com.toyboard.jeongmin.domain.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -8,12 +9,16 @@ import java.time.LocalDateTime;
 @Getter
 public class PostResponse {
 
+    @Schema(description = "게시판 id")
     private final Long id;
 
+    @Schema(description = "게시판 제목")
     private final String title;
 
+    @Schema(description = "게시판 내용")
     private final String content;
 
+    @Schema(description = "게시판 생성일자")
     private final LocalDateTime regTime;
 
     public PostResponse(Post post) {
