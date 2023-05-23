8ad1413b5bb2458b63c40199064fd45a2df4b179
JeongMin <skawjdals96@gmail.com>
2023-05-23 23:59:24 +0900

    Feat: 예상치 못한 에러에 대한 에러 핸들러 생성

--- src/main/java/com/toyboard/jeongmin/advice/PostExControllerAdvice.java
+++ src/main/java/com/toyboard/jeongmin/advice/PostExControllerAdvice.java
@@ -1,7 +1,10 @@
 package com.toyboard.jeongmin.advice;
 
 import com.toyboard.jeongmin.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +13,23 @@ public class PostExControllerAdvice {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResult> badRequestExHandle(BadRequestException e) {
-        return ResponseEntity.badRequest().body(new ErrorResult(e.getMessage(), "400"));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResult(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResult> methodArgumentExHandle(BindingResult bindingResult){
+        String errorMessage = bindingResult.getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        return ResponseEntity.badRequest().body(new ErrorResult(errorMessage));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResult> unexpectedExHandle(RuntimeException e) {
+        return ResponseEntity.internalServerError().body(new ErrorResult("예상하지 못한 오류가 발생했습니다."
+                , HttpStatus.INTERNAL_SERVER_ERROR.value()));
+    }
+
 }
