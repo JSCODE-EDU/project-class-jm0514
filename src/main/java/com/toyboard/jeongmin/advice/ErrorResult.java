eca2aa4f89ec30d08da5835ebf86347b09e8e555
JeongMin <skawjdals96@gmail.com>
2023-05-24 17:56:29 +0900

    Style: 에러 코드를 정수형 타입으로 수정

--- src/main/java/com/toyboard/jeongmin/advice/ErrorResult.java
+++ src/main/java/com/toyboard/jeongmin/advice/ErrorResult.java
@@ -7,13 +7,14 @@ import lombok.Getter;
 public class ErrorResult {
 
     @Schema(description = "오류 코드")
-    private final String error_code;
+    private final Integer error_code;
 
     @Schema(description = "오류 메시지")
     private final String message;
 
-    public ErrorResult(String message, String error_code) {
+    public ErrorResult(String message, Integer error_code) {
         this.message = message;
         this.error_code = error_code;
     }
+
 }
