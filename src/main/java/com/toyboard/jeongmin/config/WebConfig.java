a776ad49d7e3c441ff2ed5187b1f344b7f35d633
JeongMin <skawjdals96@gmail.com>
2023-06-18 10:49:56 +0900

    Feat: 커스텀 애노테이션 설정 추가

--- src/main/java/com/toyboard/jeongmin/config/WebConfig.java
+++ src/main/java/com/toyboard/jeongmin/config/WebConfig.java
@@ -1,11 +1,24 @@
 package com.toyboard.jeongmin.config;
 
+import com.toyboard.jeongmin.member.jwt.AuthenticationArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationArgumentResolver authenticationArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticationArgumentResolver);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
